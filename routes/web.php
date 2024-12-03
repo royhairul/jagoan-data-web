d0e668ae9aceccb6f98888800b78381abfada346
royhairul <royhairulanam19@gmail.com>
2024-12-03 10:49:15 +0700

    fix: change route for landing page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\LandingPageController;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Passwords\Confirm;
 use App\Livewire\Auth\Passwords\Email;
@@ -21,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+// Route for Landing Page
+Route::get('/', [LandingPageController::class, 'index'])->name('welcome');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
